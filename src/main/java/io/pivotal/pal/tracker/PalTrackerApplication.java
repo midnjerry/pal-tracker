cdc55bf2bd2b189aa1824bac0b6aa84812c42a7b
Jerry Balderas <Gerardo.Balderas@cognizant.com>
2020-05-26 00:44:09 -0500

    Update Autowiring for mysql to work on production

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,14 +1,21 @@
 package io.pivotal.pal.tracker;
 
-import com.mysql.cj.jdbc.MysqlDataSource;
 import io.pivotal.pal.tracker.repository.JdbcTimeEntryRepository;
 import io.pivotal.pal.tracker.repository.TimeEntryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
+
+    @Autowired
+    DataSource dataSource;
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
@@ -16,8 +23,20 @@ public class PalTrackerApplication {
 
     @Bean
     public TimeEntryRepository timeEntryRepository() {
+        return new JdbcTimeEntryRepository(dataSource);
+    }
+
+    /*
+    // This was original code.
+    // It injects SPRING_DATASOURCE_URL into a new DataSource
+
+    // Thanks Prajina!!!  It looks like SPRING_DATASOURCE_URL is not autowired by pivotal cloud.
+    // Autowiring the dataSource itself instead of creating an instance looks like it works.
+    @Bean
+    public TimeEntryRepository timeEntryRepository() {
         MysqlDataSource dataSource = new MysqlDataSource();
         dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
         return new JdbcTimeEntryRepository(dataSource);
     }
+     */
 }
